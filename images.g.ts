2e24a78f4b31f4322a4d6990f1a28f37947ce1f2
y7osshal <131858913+y7osshal@users.noreply.github.com>
2023-04-26 10:25:25 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Super Mario Bros 1995 world 1-4":return img`
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+1 1 1 1 1 . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . e . . . . . . . 
+. . . . . . . 2 . . . . . . . . 
+. . . . . . 2 . . . . . . . . . 
+. . . . . 2 . . . . . . . . . . 
+1 1 1 1 2 1 1 1 1 1 1 1 2 2 2 2 
+1 1 1 2 1 1 1 1 1 1 1 1 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 
+1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
